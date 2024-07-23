c38aae9230c19910911e39afb8b38b76b59f848b
Max K <TheZnat7@yandex.ru>
2024-07-23 19:13:27 +0300

    fix Slider for Ios

--- client/main.js
+++ client/main.js
@@ -1,41 +1,8 @@
 import "swiper/css/bundle";
 import { addSlider } from "./src/slider/addSlider.js";
-import { findPostId } from "./src/post/postId.js";
 import { clear } from "./src/post/clearInfoPost";
 
 clear();
 window.addEventListener("DOMContentLoaded", () => {
   addSlider();
-
-  // setTimeout(() => {
-  //   findPostId();
-  // }, 0);
 });
-
-// if (navigator.platform.match(/iPhone|iPod|iPad/)) {
-//   addSlider("ios");
-//   import("./src/slider/sliderIos.css");
-//   const { Swiper } = await import("swiper/bundle");
-//   var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: "auto",
-//     centeredSlides: true,
-//     spaceBetween: 24,
-//     grabCursor: true,
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//     breakpoints: {
-//       925: {
-//         slidesPerView: 2,
-//         spaceBetween: 10,
-//       },
-//     },
-//   });
-// } else {
-//   addSlider("NO");
-//   // const { Gallery } = await import("./src/slider/sliderClass.js");
-//   // new Gallery(document.getElementById("gallery"));
-// }
-
-// switchReviews();
--- client/src/slider/addSlider.js
+++ client/src/slider/addSlider.js
@@ -6,35 +6,80 @@ export function addSlider() {
   let dataReviews;
 
   async function addSliderJson(data) {
-    const caseCards = document.createElement("div");
-    caseCards.classList.add("case__cards");
-    caseCards.setAttribute("id", "gallery");
-
-    const swiperWrapper = document.createElement("div");
-    swiperWrapper.classList.add("gallery-line");
-
     let htmlSlider = "";
 
-    for (let i = 0; i < data.length; i++) {
-      htmlSlider += `
-        <div class="gallery-slide">
-          <div class="slide">
-            <img src="${data[i].logoUrl}" alt="логотип" class="slide__item__img--iconLogo" loading="lazy">
-            <p class="slide__item__title">${data[i].titlePost}</p>
-            <button class="bth--resetting-styles slide__item__link" value="${i}">Подробнее</button>
-          </div>
-        </div>
+    if (navigator.platform.match(/iPhone|iPod|iPad/)) {
+      // iso
+      for (let i = 0; i < data.length; i++) {
+        htmlSlider += `
+            <div class="swiper-slide">
+              <div class="slide">
+                <img src="${data[i].logoUrl}" alt="логотип" class="slide__item__img--iconLogo"
+                  loading="lazy">
+                <p class="slide__item__title">${data[i].titlePost}</p>
+                <button class="bth--resetting-styles slide__item__link" value="${i}">Подробнее</button>
+              </div>
+            </div>
       `;
+      }
+      const mySwiper = document.createElement("div");
+      mySwiper.classList.add("swiper-container", "mySwiper");
+      mySwiper.innerHTML = `
+      <div class="swiper-wrapper">
+        ${htmlSlider}
+      </div>
+      <div class="swiper-pagination"></div>
+    `;
+      document.querySelector(".test").append(mySwiper);
+      findPostId();
+      import("./sliderIos.css");
+
+      const module = await import("swiper/bundle");
+      const { Swiper } = module;
+      var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 24,
+        grabCursor: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        breakpoints: {
+          925: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+        },
+      });
+    } else {
+      for (let i = 0; i < data.length; i++) {
+        htmlSlider += `
+          <div class="gallery-slide">
+            <div class="slide">
+              <img src="${data[i].logoUrl}" alt="логотип" class="slide__item__img--iconLogo" loading="lazy">
+              <p class="slide__item__title">${data[i].titlePost}</p>
+              <button class="bth--resetting-styles slide__item__link" value="${i}">Подробнее</button>
+            </div>
+          </div>
+        `;
+      }
+      const caseCards = document.createElement("div");
+      caseCards.classList.add("case__cards");
+      caseCards.setAttribute("id", "gallery");
+
+      const swiperWrapper = document.createElement("div");
+      swiperWrapper.classList.add("gallery-line");
+
+      swiperWrapper.innerHTML = htmlSlider;
+      caseCards.append(swiperWrapper);
+      document.querySelector(".test").append(caseCards);
+      findPostId();
+
+      const module = await import("/src/slider/sliderClass.js");
+      const { Gallery } = module;
+      new Gallery(caseCards);
     }
-
-    swiperWrapper.innerHTML = htmlSlider;
-    caseCards.append(swiperWrapper);
-    document.querySelector(".test").append(caseCards);
-    findPostId();
-
-    const module = await import("/src/slider/sliderClass.js");
-    const { Gallery } = module;
-    new Gallery(caseCards);
   }
 
   async function addDataForSlider() {
