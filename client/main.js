f7e0364b05875ca45102071b2fc296374928f926
Max K <TheZnat7@yandex.ru>
2024-08-01 13:13:55 +0300

    fix

--- client/main.js
+++ client/main.js
@@ -3,6 +3,7 @@ import { addSlider } from "./src/slider/addSlider.js";
 import { clear } from "./src/post/clearInfoPost";
 
 clear();
+
 window.addEventListener("DOMContentLoaded", () => {
   addSlider();
 });
